8f7e8e928d8fb08fc900f4053d212ba85eb5cd37
Jinping Wu <jinping.wu@verisilicon.com>
2023-02-05 09:55:05 +0800

    Fix ddr memory map layout

--- board/virtual_platform/a55/a55.c
+++ board/virtual_platform/a55/a55.c
@@ -18,7 +18,7 @@ static struct mm_region a55_mem_map[] = {
 		/* RAM */
 		.virt = 0x31000000UL,
 		.phys = 0x31000000UL,
-		.size = 24 * 1024 * 1024,
+		.size = 0xf000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			 PTE_BLOCK_INNER_SHARE
 	}, {
@@ -39,6 +39,7 @@ void reset_cpu(void)
 
 int print_cpuinfo(void)
 {
+	printf("CPU:   QEMU Thomas a55\n");
 	return 0;
 }
 
